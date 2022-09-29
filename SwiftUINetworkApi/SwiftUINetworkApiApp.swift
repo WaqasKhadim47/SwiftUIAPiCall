8b7a80689ebc0fce3e8a7df8af9385b393ce5c87
waqas.khadim <waqas.khadim@invozone.com>
2022-09-29 17:49:14 +0500

    Initial Commit

---
+++ SwiftUINetworkApi/SwiftUINetworkApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINetworkApiApp.swift
+//  SwiftUINetworkApi
+//
+//  Created by Waqas Khadim on 29/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINetworkApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
